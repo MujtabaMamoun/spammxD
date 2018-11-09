733678f7ec90a4b5d406784e5bb584c91d8f48d7
MujtabaMamoun <44872348+MujtabaMamoun@users.noreply.github.com>
2018-11-09 05:09:11 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "510143396862492672"; // ايدي السررفر
-var channel = "510143396862492672";//ايدي الروم
+var server = "510143396862492672"; // 
+var channel = "510143396862492672";//
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
